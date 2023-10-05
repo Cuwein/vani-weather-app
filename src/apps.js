13e19f7d5a4f47f4a9986fae6d158562915474df
Cuwein <110616968+Cuwein@users.noreply.github.com>
2023-10-05 18:07:16 +0100

    Update apps.js

--- src/apps.js
+++ src/apps.js
@@ -1,5 +1,5 @@
 let apiKey = "dfb3ff02b5b960aft45a45103c649o5a";
 let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query={query}&key={key}";
+  "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=dfb3ff02b5b960aft45a45103c649o5a&units=metric";
 
 console.log(apiUrl);
