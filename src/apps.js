387a8a59961b3c5a9600222de0d43ed57c067511
Cuwein <110616968+Cuwein@users.noreply.github.com>
2023-10-05 17:57:37 +0100

    Update js

--- src/apps.js
+++ src/apps.js
@@ -1,1 +1,2 @@
-let apiKey = "8427d1dcabf742280d96f18aa1a4f1bc";
+let apiKey = "dfb3ff02b5b960aft45a45103c649o5a";
+let apiURL = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}`;
